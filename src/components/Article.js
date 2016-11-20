b07e1f6c97cd2a78a83e90b2dec1a9aa3ef8b413
Roman <kaboom.ua@gmail.com>
2016-11-20 22:47:02 +0200

    Update Article.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -14,6 +14,7 @@ class Article extends Component {
     render() {
         const { article } = this.props;
         const body = this.state.isOpen ? <p>{article.text}</p> : null;
+        // data - не очень хороший выбор названия
         const comments = this.state.isOpen ? <CommentsList data={article.comments}/> : null;
         return (
             <section>
